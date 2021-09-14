a2f05e33c5763bd3a84c0feb21790833129e34b7
Abdi <asharif02@gmail.com>
2021-09-14 18:38:13 -0400

    updated server code

--- server.py
+++ server.py
@@ -82,15 +82,7 @@ def accepting_connections():
             print("error accepting connections")
 
 
-# 2nd thread functions - 1) See all the clients 2) Select a client 3) Send commands to the connected client
 # Interactive prompt for sending commands
-# luka> list
-# 0 Friend-A Port
-# 1 Friend-B Port
-# 2 Friend-C Port
-# luka> select 1
-# 192.168.0.112> dir
-
 
 def start_luka():
 
